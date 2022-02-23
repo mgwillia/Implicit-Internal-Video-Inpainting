98fff323f096c793d2de62b1afd0da4c24a27498
Matt Gwilliam <mattgwilliamjr@gmail.com>
2022-02-23 14:58:57 -0500

    y

--- run_test_dev.sh
+++ run_test_dev.sh
@@ -5,15 +5,15 @@
 #SBATCH --error=outfiles/iivi.out.%j
 #SBATCH --time=36:00:00
 #SBATCH --account=abhinav
-#SBATCH --qos=high
+#SBATCH --partition=scavenger
+#SBATCH --qos=scavenger
 #SBATCH --gres=gpu:gtx1080ti:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
 
 module load cuda/11.0.3
 
-#DAVIS_FOLDERS=("aerobatics" "car-race" "carousel" "cats-car" "chamaleon" "deer" "giant-slalom" "girl-dog" "golf" "guitar-violin" "gym" "helicopter" "horsejump-stick" "hoverboard" "lock" "man-bike" "monkeys-trees" "mtb-race" "orchid" "people-sunset" "planes-crossing" "rollercoaster" "salsa" "seasnake" "skate-jump" "slackline" "subway" "tandem" "tennis-vest" "tractor")
-DAVIS_FOLDERS=("bike-packing" "blackswan" "bmx-trees" "breakdance" "camel" "car-roundabout" "car-shadow" "cows" "dance-twirl" "dog" "dogs-jump" "drift-chicane" "drift-straight" "goat" "gold-fish" "horsejump-high" "india" "judo" "kite-surf" "lab-coat" "libby" "loading" "mbike-trick" "motocross-jump" "paragliding-launch" "parkour" "pigs" "scooter-black" "shooting" "soapbox")
+DAVIS_FOLDERS=("blackswan")
 
 srun bash -c "mkdir -p /scratch0/mgwillia/DAVIS;"
 srun bash -c "mkdir -p /scratch0/mgwillia/DAVIS_ANNOS;"
@@ -25,12 +25,9 @@ srun bash -c "ls /scratch0/mgwillia/DAVIS;"
 
 srun bash -c "hostname;"
 for FOLDER in ${DAVIS_FOLDERS[@]}; do
-    srun bash -c "echo $lr;"
     srun bash -c "mkdir -p /vulcanscratch/mgwillia/Implicit-Internal-Video-Inpainting/results/$FOLDER;"
     srun bash -c "mkdir -p /scratch0/mgwillia/DAVIS_MASKS/$FOLDER;"
-    #srun bash -c "ls /scratch0/mgwillia/DAVIS_ANNOS/$FOLDER;"
     srun bash -c "python scripts/preprocess_mask.py --annotation-path /scratch0/mgwillia/DAVIS_ANNOS/$FOLDER --mask-path /scratch0/mgwillia/DAVIS_MASKS/$FOLDER;"
-    #srun bash -c "ls /scratch0/mgwillia/DAVIS_MASKS/$FOLDER;"
     srun bash -c "python train.py --log-dir logs/$FOLDER --dir-video /scratch0/mgwillia/DAVIS/$FOLDER --dir-mask /scratch0/mgwillia/DAVIS_MASKS/$FOLDER;"
     srun bash -c "python test.py --test-dir /vulcanscratch/mgwillia/Implicit-Internal-Video-Inpainting/results/$FOLDER --dir-video /scratch0/mgwillia/DAVIS/$FOLDER --dir-mask /scratch0/mgwillia/DAVIS_MASKS/$FOLDER;"
 done
