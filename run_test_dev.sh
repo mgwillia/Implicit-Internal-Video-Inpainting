57fce78a398a0787b795fc5f395291804ec56330
Matt Gwilliam <mattgwilliamjr@gmail.com>
2022-02-28 16:18:54 -0500

    y

--- run_test_dev.sh
+++ run_test_dev.sh
@@ -5,8 +5,7 @@
 #SBATCH --error=outfiles/iivi.out.%j
 #SBATCH --time=36:00:00
 #SBATCH --account=abhinav
-#SBATCH --partition=scavenger
-#SBATCH --qos=scavenger
+#SBATCH --qos=high
 #SBATCH --gres=gpu:gtx1080ti:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
